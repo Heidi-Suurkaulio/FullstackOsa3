7b44705434d903746ba0196de562f77ff0e40bc7
Heidi-Suurkaulio <heidisuur@gmail.com>
2025-01-11 11:26:42 +0200

    tehtava 3.13

--- index.js
+++ index.js
@@ -1,40 +1,21 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
+
+const Persons = require('./models/person')
 
 morgan.token('type', function(request, response) {
     return JSON.stringify(request.body)
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
 
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": "1"
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": "2"
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": "3"
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": "4"
-    }
-]
-
-const ln = persons.length
+const ln = Persons.length
 const now = new Date()
 
 app.get('/info', (request, response) => {
@@ -43,18 +24,21 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Persons.find({}).then(pe => {
+        response.json(pe)
+      })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(p => p.id === id)
-    if (person) {
-        response.json(person)
-    }
-    response.status(404).end()
+    Persons.findById(request.params.id).then(p => 
+        response.json(p)
+    ).catch(err => {
+        console.log('id not found', err.message)
+        response.status(404).end()
+    })
 })
 
+// TODO next ones are not working
 app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
     persons = persons.filter(p => p.id !== id)
@@ -110,7 +94,7 @@ app.post('/api/persons', (request, response) => {
     response.json(nPerson)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
---
+++ models/person.js
@@ -0,0 +1,28 @@
+const mongo = require('mongoose')
+
+mongo.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+mongo.connect(url)
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
+
+const newSchema = new mongo.Schema({
+    name: String,
+    number: String
+})
+
+newSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongo.model('Person', newSchema)
